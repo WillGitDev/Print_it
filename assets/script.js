effef89289305c3df675c4d56fcbaa8d647f078b
wil974-dev <wggron99@gmail.com>
2025-10-03 13:53:42 +0400

    Impémentation des événements sur les composants de navigation, avec gestion du carrousel d'images et des indicateurs(bullet point)

--- assets/script.js
+++ assets/script.js
@@ -20,17 +20,44 @@ const slides = [
 const arrow_left = document.getElementsByClassName("arrow_left")[0];
 const arrow_right = document.getElementsByClassName("arrow_right")[0];
 const dots = document.getElementsByClassName("dots")[0];
+const dotsList = [];
+const banner_img = document.getElementsByClassName("banner-img")[0];
+const tagLine = document.querySelector("#banner p");
+const pathImg = "assets/images/slideshow/";
+let currentIndex = 0;
+
 
 arrow_left.addEventListener("click", () => {
-	console.log("Fléche gauche cliqué");
+	currentIndex = (currentIndex <= 0) ? slides.length - 1 : currentIndex - 1;
+	banner_img.setAttribute("src", `${pathImg}${slides[currentIndex].image}`);
+	tagLine.innerHTML = slides[currentIndex].tagLine;
+	dotSelected(currentIndex);
 });
 
 arrow_right.addEventListener("click", () => {
-	console.log("Flèche droite cliqué");
+	currentIndex = (currentIndex >= slides.length -1) ? 0 : currentIndex + 1;
+	banner_img.setAttribute("src", `${pathImg}${slides[currentIndex].image}`);
+	tagLine.innerHTML = slides[currentIndex].tagLine;
+	dotSelected(currentIndex);
 });
 
-slides.forEach(() => {
+slides.forEach((_, index) => {
 	const dot = document.createElement("div");
 	dot.classList.add("dot");
+	if(index === 0){dot.classList.add("dot_selected")};
 	dots.appendChild(dot);
-})
+	dotsList.push(dot);
+});
+
+/**
+ * Permet d'attribuer la classe dot_selected sur
+ * le bullet point en fonction de l'image affichée dans le
+ * carrousel
+ * @param {number} index Index de l'image en cours.
+ */
+function dotSelected(index){
+	dotsList.forEach((dot) => {
+		dot.classList.remove("dot_selected");
+	});
+	dotsList[index].classList.add("dot_selected");
+};
